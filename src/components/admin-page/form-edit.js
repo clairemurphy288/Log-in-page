5f6163979674e29af3bdb83e3e5d6593826af4c4
Claire <murphyclaire91@gmail.com>
2022-07-26 10:17:15 -0400

    Got data compiled for each form now needs to be sent to backend

--- src/components/admin-page/form-edit.js
+++ src/components/admin-page/form-edit.js
@@ -1,20 +1,27 @@
 import react from 'react';
 import React from 'react';
+import {useState, useEffect} from 'react';
+
 export default function Form(props) {
     const input = React.useRef();
+    const [data, setData] = useState([]);
+    const [question, setQuestion] = useState(props.question)
     //props I need _id, question
 
     function onChange(e) {
-        console.log(input.current.value);
-        // console.log(textInput);
+        setQuestion(input.current.value);
+        console.log(question);
+        console.log(data);
 
     }
-    console.log(props.answerChoices)
-    const answerItems = props.answerChoices.map(answer => (<Answer answer={answer}/>))
-    console.log(answerItems);
+    function onSubmit(e) {
+        e.preventDefault();
+        //props.id for question id
+    }
+    const answerItems = props.answerChoices.map((answer,index) => (<Answer setData={setData} data={data} onChange = {onChange} answerChoices={props.answerChoices} id={index} answer={answer}/>))
     return (
 <div>
-  <form>
+  <form onSubmit={onSubmit}>
   <textarea ref={input} onChange={onChange}  defaultValue = {props.question} cols="50" rows="10"></textarea>
     <div>{answerItems}</div>
     <button type="submit">Submit Changes</button>
@@ -25,17 +32,40 @@ export default function Form(props) {
     )
 
 
+
+
 }
 
 function Answer(props) {
     const textInput = React.useRef();
+    // const [answers, setAnswers] = useState(props.answerChoices);
+    // const [index, setIndex] = useState(props.id);
     function onChange(e) {
-        console.log(textInput.current.value);
+        let deepCopy = [];
+        if ( props.data.length === 0 ) {
+            for (let i = 0; i < props.answerChoices.length; i++) {
+                deepCopy.push(props.answerChoices[i])
+            }
+        } else {
+            for (let i = 0; i < props.data.length; i++) {
+                deepCopy.push(props.data[i])
+        }
+    }
+        deepCopy[props.id] = textInput.current.value
+        props.setData(deepCopy);
+        console.log(props.data)
+
 
     }
+
+
     return (
         <input onChange={onChange} ref={textInput} type = "text" defaultValue={props.answer}></input>
       )
 }
+
+
+
+
 
 
