61cb530c3b9afb5889c4a71abd0911472d635ce0
Claire <murphyclaire91@gmail.com>
2022-07-27 11:45:53 -0400

    fixed arrow keys and edit functionality

--- src/components/admin-page/form-edit.js
+++ src/components/admin-page/form-edit.js
@@ -2,23 +2,26 @@ import react from 'react';
 import React from 'react';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+import "./form.css";
 
 export default function Form(props) {
     const input = React.useRef();
     const [data, setData] = useState([]);
     const [question, setQuestion] = useState(props.question)
+    const [selectedAnswer, setAnswer] = useState(props.indexOfAnswer);
     //props I need _id, question
 
     function onChange(e) {
         setQuestion(input.current.value);
         console.log(question);
         console.log(data);
+        console.log(selectedAnswer);
 
     }
     async function onSubmit(e) {
         e.preventDefault();
         console.log(question)
-        await axios.post('http://localhost:5000/admin/edit/quiz', {quizId: props.quizId, question: question, data: data, id: props.id}).then(async res => {
+        await axios.post('http://localhost:5000/admin/edit/quiz', {indexOfAnswer: selectedAnswer, quizId: props.quizId, question: question, data: data, id: props.id}).then(async res => {
             console.log(res.data);
 
            }).catch(err => console.log(err));
@@ -26,11 +29,13 @@ export default function Form(props) {
           
         //props.id for question id
     }
-    const answerItems = props.answerChoices.map((answer,index) => (<Answer setData={setData} data={data} onChange = {onChange} answerChoices={props.answerChoices} id={index} answer={answer}/>))
+    const answerItems = props.answerChoices.map((answer,index) => (<Answer selectedAnswer={selectedAnswer} setAnswer={setAnswer} setData={setData} data={data} onChange = {onChange} answerChoices={props.answerChoices} index={index} answer={answer}/>))
     return (
 <div>
   <form onSubmit={onSubmit}>
   <textarea ref={input} onChange={onChange}  defaultValue = {props.question} cols="50" rows="10"></textarea>
+  
+        <h6>Select the correct answer: </h6>
     <div>{answerItems}</div>
     <button type="submit">Submit Changes</button>
     <hr></hr>
@@ -45,7 +50,10 @@ export default function Form(props) {
 }
 
 function Answer(props) {
+    const [style, setStyle] = useState("")
     const textInput = React.useRef();
+    const [clicked, setClick] = useState(false);
+    
     function onChange(e) {
         let deepCopy = [];
         if ( props.data.length === 0 ) {
@@ -57,16 +65,40 @@ function Answer(props) {
                 deepCopy.push(props.data[i])
         }
     }
-        deepCopy[props.id] = textInput.current.value
+        deepCopy[props.index] = textInput.current.value
         props.setData(deepCopy);
         console.log(props.data)
 
 
     }
+    //function allows the admin to change the correct answer 
+    function onClick() {
+        
+        if (props.selectedAnswer === props.index) {
+            setStyle("");
+            setClick(true);
+            props.setAnswer(-1);
+            //remove class
+            console.log("selected");
+        } else if(props.selectedAnswer === -1) {
+            setClick(true);
+            props.setAnswer(props.index);
+            setStyle("select");
+
+        }
+    }
+
+    useEffect(() => {
+        if (props.selectedAnswer === props.index  && !clicked) {
+            setStyle(style + "select");
+        } 
+
+    },[props.selectedAnswer])
 
 
     return (
-        <input onChange={onChange} ref={textInput} type = "text" defaultValue={props.answer}></input>
+
+            <input className = {style} onClick={onClick} onChange={onChange} ref={textInput} type = "text" defaultValue={props.answer}></input>
       )
 }
 
